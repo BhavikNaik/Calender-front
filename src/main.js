c357be4033279398a443f22f5d4fd3b79f296ca5
Bhavik <naikbhavik9@gmail.com>
2024-02-13 01:58:45 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,43 +10,65 @@ import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
 import 'v-calendar/style.css';
+// import { Kalendar } from 'kalendar-vue';
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+import VueCookies from 'vue-cookies'
 //import CalenderVue from './components/Calender.vue'
 
 const app = createApp(App)
 
 app.use(setupCalendar, {})
 app.use(Toast)
+app.use(VueCookies);
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: reg },
         { path: '/login', component: login, name: 'Login' },
-        { path: '/home', component: HomeVue },
+        { path: '/home', component: HomeVue, meta: { requiresAuth: true }},
         // meta: { requiresAuth: true }
     ]
 });
 app.use(router);
 
-// router.beforeEach(async (to, from, next) => {
-//     if (to.matched.some(record => record.meta.requiresAuth)) {
-//         try {
-//             // Check if the user is authenticated
-//             const response = await axios.get('http://localhost:3000/check-auth'); // You need to implement this endpoint in your backend
-//             if (response.data.isAuthenticated) {
-//                 next();
-//             } else {
-//                 next({ name: 'Login' });
-//             }
-//         } catch (error) {
-//             // An error occurred while checking authentication status
-//             console.error('Error checking authentication status:', error);
-//             next({ name: 'Login' });
-//         }
-//     } else {
-//         next();
-//     }
-// });
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        try {
+            // Checking if user is authenticated
+
+            // if(VueCookies.isKey('email')){
+            //     next();
+            // }else{
+            //     next({ name: 'Login' });
+            // }
+
+            const response = await axios.get(`http://localhost:3000/home`,{withCredentials:true});
+            if (response.data.success) {
+                console.log("hai bhai",response)
+                next();
+            } else {
+                console.log("thoda problem hai bhai",response)
+                next({ name: 'Login' });
+            }
+
+            // .then((response) => {
+            //     console.log("hua",response)
+            // }).catch((err) => {
+            //     console.log("nai",err)
+            // })
+        } catch (error) {
+            // An error occurred while checking authentication status
+            // console.log("issue",response)
+            console.error('Error checking authentication status:', error);
+            next({ name: 'Login' });
+        }
+    } else {
+        // console.log("nahi hai")
+        next();
+    }
+});
 
 
 // Use the components
